634e0a783da423772ebff294c491626baaf29441
Sahithi-Akunuri <111894343+Sahithi-Akunuri@users.noreply.github.com>
2024-12-25 17:54:58 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,27 @@
+const studentID = 329
+let studentName = "Sahithi"
+var studentPhone = 7093165555
+studentCity = "Vijayawada"
+let studentState;
+
+// console.log(studentID);
+// console.log(studentName);
+// console.log(studentPhone);
+// console.log(studentCity); //can use table
+
+// studentID = 330 //not allowed
+studentName = "Jay"
+studentPhone = 7893339444
+studentCity = "Bangalore"
+
+// declaration of constant -> 1 way -> const
+// declaration of variable -> 2 ways -> let, var
+// difference between let and var -> Scope{ } => var->no scope, let->scope
+// JS is a safe language that we can use a variable without let and var -> bad declaration
+// The variables which are not initialized are by default declared as "undefined"
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([studentID, studentName, studentPhone, studentCity, studentState])
